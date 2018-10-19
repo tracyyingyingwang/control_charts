e8abf2f75ff901d5e393a6873dc90e81e0ac6af5
Gilles Pilon <gilles.pilon@gillespilon.ca>
2018-10-19 08:29:11 -0500

    Minor edits

--- x_mr_control_charts_example.py
+++ x_mr_control_charts_example.py
@@ -22,6 +22,8 @@ ax1 = x.ax
 ax1.set_title('X control chart' + '\n' 'Subtitle')
 ax1.set_ylabel('Response (units)')
 ax1.set_xlabel('X axis label')
+plt.show()
+#plt.clf()
 mr = mR(chart_data)
 print('mR chart')
 print('Upper control limit', mr.ucl, sep=' = ')
